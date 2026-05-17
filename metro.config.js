d9e2e7895f7ea3153bba8dffbf235a2826b97943
jrrock26 <jrrock26@gmail.com>
2026-05-17 11:25:31 -0400

    Use @react-native/metro-config for RN 0.73 and extend assetExts

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,18 @@
 // metro.config.js
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
+/**
+ * Metro configuration
+ * https://metrobundler.dev/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
 const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(defaultConfig, {
+const config = {
   resolver: {
     assetExts: [...defaultConfig.resolver.assetExts, 'mp3', 'wav'],
   },
-});
+};
+
+module.exports = mergeConfig(defaultConfig, config);

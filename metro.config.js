e036a41214d1245202721617208261fdd05c0e1a
jrrock26 <jrrock26@gmail.com>
2026-05-17 11:05:49 -0400

    Fix Metro config for RN 0.73 (async getDefaultConfig)

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,12 @@
 // metro.config.js
 const { getDefaultConfig } = require('metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig(__dirname);
 
-module.exports = {
-  resolver: {
-    assetExts: [...defaultConfig.resolver.assetExts, 'mp3', 'wav'],
-  },
-};
+  return {
+    resolver: {
+      assetExts: [...defaultConfig.resolver.assetExts, 'mp3', 'wav'],
+    },
+  };
+})();

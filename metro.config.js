32a909e627633198ebb9bc43a88819c86f8a6313
jrrock26 <jrrock26@gmail.com>
2026-05-17 10:48:15 -0400

    Force Metro to use correct config via METRO_CONFIG override in Xcode build script

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,10 @@
 // metro.config.js
 const { getDefaultConfig } = require('metro-config');
 
-module.exports = (async () => {
-  const defaultConfig = await getDefaultConfig();
+const defaultConfig = getDefaultConfig(__dirname);
 
-  return {
-    resolver: {
-      assetExts: [...defaultConfig.resolver.assetExts, 'mp3', 'wav'],
-    },
-  };
-})();
+module.exports = {
+  resolver: {
+    assetExts: [...defaultConfig.resolver.assetExts, 'mp3', 'wav'],
+  },
+};
